934e24db492258f230802cf9526786e994a5aab8
Lucas Leung <kayauleung2024@gmail.com>
2025-11-01 15:42:11 +0800

    Finish const lesson

---
+++ lesson-3/const.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    const double pie = 3.141592653589793238462643383279502884197169399275105820;
+    const int radius = 2;
+    const double area = pie*radius*2;
+    std::cout << area << '\n';
+    return 0;
+}
