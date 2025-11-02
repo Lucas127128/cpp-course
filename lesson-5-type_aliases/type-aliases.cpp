6e17c69e33fdb931be3bbe6755530c45e6e7def4
Lucas Leung <kayauleung2024@gmail.com>
2025-11-02 18:25:26 +0800

    Finish the type aliases lesson and rename the folder

---
+++ lesson-5-type_aliases/type-aliases.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//typedef std::string string_t;
+using string_t = std::string;
+
+int main() {
+    string_t firstName = "Lucas";
+    std:: cout << firstName << '\n';
+
+    return 0;
+}
